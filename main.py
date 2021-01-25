fd2cf4168f2ab4e0cb7c8641fae1ddb871b87c03
christopher keene <batman313rd@gmail.com>
2021-01-25 19:51:13 +0000

    added insult on thumbs down and cannot insult himself

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ logger = logging.getLogger()
 
 client = discord.Client()
 
+
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " - " + json_data[0]['a']
     print("response: ", json_data)
     return (quote)
+
 
 def get_insult():
     response = requests.get(
@@ -28,33 +30,31 @@ def get_insult():
     quote = json_data['insult']
     return (quote)
 
+
 def get_wit_message(wit_message):
-    print('just trying to show something, boys, boys, boys: ' + str(wit_message))
+    print('just trying to show something, boys, boys, boys: ' +
+          str(wit_message))
     resp = wit_client.message(wit_message)
     print('Yay, got Wit.ai response: ' + str(resp))
     return (resp)
+
 
 @client.event
 async def on_ready():
     print('we have logged in  {0.user}'.format(client))
 
+
 @client.event
 async def on_reaction_add(reaction, user):
     print('reaction: ' + str(reaction))
-    print('reaction: ' + str(user))
-    print('reaction: ' + str(client.user))
-    if reaction.emoji == '💩' and reaction.message.author != client.user:
+    print('reaction author: ' + str(reaction.message.author))
+    print('client.user: ' + str(client.user))
+    print('is this true: ' + str((reaction.emoji == '💩' or reaction.emoji == '👎🏿') and (reaction.message.author != client.user)))
+    if (reaction.emoji == '💩' or reaction.emoji == '👎🏿') and (reaction.message.author != client.user):
         print('this worked with the unicode')
         insult = get_insult()
-        await reaction.message.channel.send(reaction.message.author.mention + insult)
-        
-        # await reaction.message.channel.send('@{} '.format(reaction.message.author.name) + insult)
-        
-@client.event
-async def on_raw_reaction_add(payload):
-    print('payload: ' + str(payload))
-    if payload.emoji == '👍':
-        print('this worked!')
+        await reaction.message.channel.send(reaction.message.author.mention + ' ' + insult)
+        print('done')
 
 @client.event
 async def on_message(message):
@@ -74,21 +74,9 @@ async def on_message(message):
     if message.content.startswith('$what is'):
         message = get_wit_message(discord_message)
 
-    # if discord_chat.text
-    # if message.content.startswith('$test_quote'):
-    #     quote = function_name(message.content)
-    #     await message.channel.send(quote)
-
     if message.content.startswith('$inspire'):
         quote = get_quote()
         await message.channel.send(quote)
-
-    # if any(word in massage.content for word in who_is())
-
-    # if message.content.startswith('$yo'):
-    #     reply = who_is()
-    #     await message.channel.send(quote)
-
 
 # client.run(os.getenv('TOKEN'))
 TOKEN = os.getenv('TOKEN')
