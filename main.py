3d8beb72a9eec7931087623a31319fbf1e793da0
christopher keene <batman313rd@gmail.com>
2021-01-25 22:10:33 +0000

    added reaction count to prevent spamming

--- main.py
+++ main.py
@@ -13,29 +13,26 @@ logger = logging.getLogger()
 
 client = discord.Client()
 
+reaction_count = []
+
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " - " + json_data[0]['a']
-    print("response: ", json_data)
     return (quote)
 
 
 def get_insult():
     response = requests.get(
         "https://evilinsult.com/generate_insult.php?lang=en&type=json")
-    print(response.text)
     json_data = json.loads(response.text)
     quote = json_data['insult']
     return (quote)
 
 
 def get_wit_message(wit_message):
-    print('just trying to show something, boys, boys, boys: ' +
-          str(wit_message))
     resp = wit_client.message(wit_message)
-    print('Yay, got Wit.ai response: ' + str(resp))
     return (resp)
 
 
@@ -43,21 +40,21 @@ def get_wit_message(wit_message):
 async def on_ready():
     print('we have logged in  {0.user}'.format(client))
 
-
 @client.event
 async def on_reaction_add(reaction, user):
-    print('reaction: ' + str(reaction))
-    print('reaction author: ' + str(reaction.message.author))
-    print('client.user: ' + str(client.user))
-    print('is this true: ' + str((reaction.emoji == '💩' or reaction.emoji == '👎🏿') and (reaction.message.author != client.user)))
-    if (reaction.emoji == '💩' or reaction.emoji == '👎🏿') and (reaction.message.author != client.user):
-        print('this worked with the unicode')
+    print('reaction count: ' + str(len(reaction_count)))
+    # reaction_count = reaction.message.reactions
+    if (reaction.emoji == '💩' or reaction.emoji == '👎🏿') and (reaction.message.author != client.user and len(reaction_count) < 1):
+        reaction_count.append(reaction.emoji)
         insult = get_insult()
+        print('reaction count 2: ' + str(len(reaction_count)))
         await reaction.message.channel.send(reaction.message.author.mention + ' ' + insult)
-        print('done')
 
 @client.event
 async def on_message(message):
+    if message.author != client.user:
+        reaction_count.clear()
+        
     discord_message = message.content
     # discord_chat = get_wit_message(discord_message)
 
